713953f43427bd4bfd0b3bba4d1435af57ed5597
Pedro Monteiro <pedromalvesmonteiro@gmail.com>
2018-02-04 20:04:11 +0100

    Added fragments

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,10 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by pedro on 04/02/2018.
+ */
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+}
